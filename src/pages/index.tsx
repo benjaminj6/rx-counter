3d24c521231db2fec8ef99c1df8e3ce4ef163f98
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2018-03-26 22:10:54 -0700

    Integrate rxjs state store with bound handlers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react'
 import Counter from '../components/Counter'
 
+import { dispatch, State, store$ } from '../rx/counter'
+
+const a = store$.subscribe(state => console.log(state))
+
+const increment = dispatch(() => ({ type: 'INC' }))
+const decrement = dispatch(() => ({ type: 'DEC' }))
+
+// todo -- bind the store to a react instance...perhaps with a component & render prop?
 const IndexPage = () => (
   <div className="p-4">
     <Counter
       count={2}
-      inc={() => console.log('inc')}
-      dec={() => console.log('dec')}
+      inc={increment}
+      dec={decrement}
     />
   </div>
 )
--- src/rx/counter.ts
+++ src/rx/counter.ts
@@ -1,8 +1,40 @@
+import { Subject } from 'rxjs/Subject'
+import { Observable } from 'rxjs/Observable'
+import { scan } from 'rxjs/operators'
+
+const INC = 'INC'
+const DEC = 'DEC'
+
+type Action = {
+  type: string
+  count?: number
+}
+
+type ActionCreator = (...args: any[]) => Action
+
 export type State = {
-  count: Number
+  count: number
 }
 
 export const initialState: State = {
-  count: 0
+  count: 0,
 }
 
+export const action$ = new Subject()
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case INC:
+      return { count: state.count + 1 }
+    case DEC: {
+      if (state.count === 0) return state;
+      return { count: state.count - 1 }
+    }
+    default:
+      return state
+  }
+}
+
+export const dispatch = (fn: ActionCreator) => (...args: any[]) =>
+  action$.next(fn(...args))
+export const store$ = action$.pipe(scan(reducer, initialState))
